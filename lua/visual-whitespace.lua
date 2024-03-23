a23d6d4ef385d9b5aee0ad57cf7cd6d718a4e20d
McAuley Penney <jacobmpenney@gmail.com>
2024-03-22 20:30:40 -0700

    refactor: get marks, then apply in batches
    - rename functions for clarity
    - rename cfg table to reflect global scope
    - rm `set_mark()` in favor of `apply_marks()`
    - modularize functionality to clean up main

--- lua/visual-whitespace.lua
+++ lua/visual-whitespace.lua
@@ -3,20 +3,13 @@ local fn = vim.fn
 
 local M = {}
 local NS_ID = api.nvim_create_namespace('VisualWhitespace')
-
-local cfg = {
+local CFG = {
   highlight = { link = "Visual" },
   space_char = '·',
   tab_char = '→',
   nl_char = '↲'
 }
 
-local function set_mark(row, col, text)
-  api.nvim_buf_set_extmark(0, NS_ID, row - 1, col - 1, {
-    virt_text = { { text, 'VisualNonText' } },
-    virt_text_pos = 'overlay',
-  })
-end
 
 local function get_charwise_pos(s_pos, e_pos)
   local srow, scol = s_pos[2], s_pos[3]
@@ -44,15 +37,65 @@ local function get_linewise_pos(s_pos, e_pos)
   return { srow, scol }, { erow, ecol }
 end
 
-M.mark_ws = function()
+local function get_marks(s_pos, e_pos, mode)
+  local ff = vim.bo.fileformat
+  local nl_str = ff == 'unix' and '\n' or ff == 'mac' and '\r' or '\r\n'
+
+  local srow, scol = s_pos[1], s_pos[2]
+  local erow, ecol = e_pos[1], e_pos[2]
+
+  local text = api.nvim_buf_get_lines(0, srow - 1, erow, true)
+
+  local line_text, select_scol, select_ecol, cur_char
+  local ws_marks = {}
+  for cur_row = srow, erow do
+    -- gets the physical line, not the display line
+    line_text = table.concat { text[cur_row - srow + 1], nl_str }
+
+    -- adjust start_col and end_col for partial line selections
+    if mode == 'v' then
+      select_scol = (cur_row == srow) and scol or 1
+      select_ecol = (cur_row == erow) and ecol or #line_text
+    else
+      select_scol = scol
+      select_ecol = #line_text
+    end
+
+    for cur_col = select_scol, select_ecol do
+      cur_char = line_text:sub(cur_col, cur_col)
+
+      if cur_char == ' ' then
+        table.insert(ws_marks, { cur_row, cur_col, CFG['space_char'] })
+      elseif cur_char == '\t' then
+        table.insert(ws_marks, { cur_row, cur_col, CFG['tab_char'] })
+      elseif cur_char == nl_str then
+        table.insert(ws_marks, { cur_row, cur_col, CFG['nl_char'] })
+      end
+    end
+  end
+
+  return ws_marks
+end
+
+local function apply_marks(mark_table)
+  for _, mark_data in ipairs(mark_table) do
+    api.nvim_buf_set_extmark(0, NS_ID, mark_data[1] - 1, mark_data[2] - 1, {
+      virt_text = { { mark_data[3], 'VisualNonText' } },
+      virt_text_pos = 'overlay',
+    })
+  end
+end
+
+M.clear_ws_hl = function()
+  api.nvim_buf_clear_namespace(0, NS_ID, 0, -1)
+end
+
+M.highlight_ws = function()
   local cur_mode = fn.mode()
 
   if cur_mode ~= 'v' and cur_mode ~= 'V' then
     return
   end
-
-  local ff = vim.bo.fileformat
-  local nl_str = ff == 'unix' and '\n' or ff == 'mac' and '\r' or '\r\n'
 
   local s_pos = fn.getpos('v')
   local e_pos = fn.getpos('.')
@@ -63,52 +106,18 @@ M.mark_ws = function()
     s_pos, e_pos = get_linewise_pos(s_pos, e_pos)
   end
 
-  M.clear_marked_ws()
+  M.clear_ws_hl()
 
-  local srow, scol = s_pos[1], s_pos[2]
-  local erow, ecol = e_pos[1], e_pos[2]
+  local marks = get_marks(s_pos, e_pos, cur_mode)
 
-  local text = api.nvim_buf_get_lines(0, srow - 1, erow, true)
-  local text_i = 1
-
-  for cur_row = srow, erow do
-    -- gets the physical line, not the display line
-    local line_text = text[text_i] .. nl_str
-
-    -- adjust start_col and end_col for partial line selections
-    local select_scol, select_ecol
-    if cur_mode == 'v' then
-      select_scol = (cur_row == srow) and scol or 1
-      select_ecol = (cur_row == erow) and ecol or #line_text
-    else
-      select_scol = scol
-      select_ecol = #line_text
-    end
-
-    for cur_col = select_scol, select_ecol do
-      local cur_char = line_text:sub(cur_col, cur_col)
-
-      if cur_char == ' ' then
-        set_mark(cur_row, cur_col, cfg['space_char'])
-      elseif cur_char == '\t' then
-        set_mark(cur_row, cur_col, cfg['tab_char'])
-      elseif cur_char == nl_str then
-        set_mark(cur_row, cur_col, cfg['nl_char'])
-      end
-    end
-
-    text_i = text_i + 1
-  end
-end
-
-M.clear_marked_ws = function()
-  api.nvim_buf_clear_namespace(0, NS_ID, 0, -1)
+  apply_marks(marks)
 end
 
 M.setup = function(user_cfg)
-  cfg = vim.tbl_extend('force', cfg, user_cfg or {})
+  CFG = vim.tbl_extend('force', CFG, user_cfg or {})
 
-  api.nvim_set_hl(0, 'VisualNonText', cfg['highlight'])
+  api.nvim_set_hl(0, 'VisualNonText', CFG['highlight'])
 end
+
 
 return M
--- plugin/visual-whitespace.lua
+++ plugin/visual-whitespace.lua
@@ -5,18 +5,18 @@ local augrp = vim.api.nvim_create_augroup("VisualWhitespace", { clear = true })
 aucmd("ModeChanged", {
   group = augrp,
   pattern = "*:[vV]",
-  callback = match_vis.mark_ws
+  callback = match_vis.highlight_ws
 })
 
 aucmd("CursorMoved", {
   group = augrp,
   callback = function()
-    return vim.schedule(match_vis.mark_ws)
+    return vim.schedule(match_vis.highlight_ws)
   end
 })
 
 aucmd("ModeChanged", {
   group = augrp,
   pattern = "[vV]:*",
-  callback = match_vis.clear_marked_ws
+  callback = match_vis.clear_ws_hl
 })
